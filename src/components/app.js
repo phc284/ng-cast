01554d1d149da0118edf8500d995f5e8074d905d
Hack Reactor Student <student@hackreactor.com>
2017-09-01 20:09:15 -0500

    working on api search

--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,26 @@ angular.module('video-player')
 .component('app', {
   templateUrl: 'src/templates/app.html',
 
+  controller: function(youTube) {
 
-  controller: function() {
+    console.log(youTube);
+    // this.videos = youTube($http)
     this.videos = exampleVideoData;
     this.currentVideo = this.videos[0];
+
     this.onClick = function(index) {
       this.currentVideo = this.videos[index];
     }.bind(this);
-    // this.onClick = this.onClick.bind(this);
 
     this.search = function (query) {
       console.log(query);
+      youTube(query);
     }.bind(this);
+
+
+    // $scope.data = youTube.data();
+
+
+
   }
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,45 @@
 angular.module('video-player')
 .service('youTube', function(){
-  // TODO
+
+  this.search = function(query){
+  };
+
+  // return {};
 });
+
+
+
+
+  // return {
+  //   getData: function(){
+  //     $http({
+  //
+  //     })
+  //   },
+  //   data: function(){ return myData; }
+  // }
+  //
+  //
+  //
+  // this.getVideos = function(query){
+  //
+  //
+  //
+  //   $http({
+  //     methods: 'GET',
+  //     url: 'https://www.googleapis.com/youtube/v3/search',
+  //     key : YOUTUBE_API_KEY,
+  //     videoEmbeddable : 'true',
+  //     type : 'video',
+  //     part: 'snippet',
+  //     q : query,
+  //     maxResults : 5
+  //   })
+  //
+  //   .then(function successCallback(response)){
+  //       this.videos = response.items;
+  //     }, function errorCallback(response){
+  //       console.log('error: ', response);
+  //     });
+  // }
+  //
