885fd347cf6384c39f7f4d4614e34963ec549287
Peter Choi <phc284@gmail.com>
2017-09-02 10:36:07 -0500

    getting videos from youtube

--- src/components/app.js
+++ src/components/app.js
@@ -1,27 +1,27 @@
 angular.module('video-player')
-.component('app', {
-  templateUrl: 'src/templates/app.html',
+  .component('app', {
+    templateUrl: 'src/templates/app.html',
 
-  controller: function(youTube) {
+    controller: function(youTube) {
 
-    console.log(youTube);
-    // this.videos = youTube($http)
-    this.videos = exampleVideoData;
-    this.currentVideo = this.videos[0];
+      console.log(youTube);
+      // this.videos = youTube($http)
+      this.videos = exampleVideoData;
+      this.currentVideo = this.videos[0];
 
-    this.onClick = function(index) {
-      this.currentVideo = this.videos[index];
-    }.bind(this);
+      this.onClick = function(index) {
+        this.currentVideo = this.videos[index];
+      }.bind(this);
 
-    this.search = function (query) {
-      console.log(query);
-      youTube(query);
-    }.bind(this);
-
-
-    // $scope.data = youTube.data();
+      this.search = function (query) {
+        //console.log(query);
+        youTube.search(query);
+      }.bind(this);
 
 
+      // $scope.data = youTube.data();
 
-  }
-});
+
+
+    }
+  });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,45 +1,59 @@
 angular.module('video-player')
-.service('youTube', function() {
+  .service('youTube', function($http) {
 
-  this.search = function(query) {
-  };
+    this.search = function(query, callback) {
+      $http.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          part: 'snippet',
+          maxResults: 5,
+          key: window.YOUTUBE_API_KEY,
+          videoEmbeddable: 'true',
+          type: 'video',
+          q: query
+        }
+      })
+        .then(function (data) {
+          console.log(data.data.items);
+          callback(data.items);
+        });
+    };
 
-  // return {};
-});
+    // return {};
+  });
 
 
 
 
-  // return {
-  //   getData: function(){
-  //     $http({
-  //
-  //     })
-  //   },
-  //   data: function(){ return myData; }
-  // }
-  //
-  //
-  //
-  // this.getVideos = function(query){
-  //
-  //
-  //
-  //   $http({
-  //     methods: 'GET',
-  //     url: 'https://www.googleapis.com/youtube/v3/search',
-  //     key : YOUTUBE_API_KEY,
-  //     videoEmbeddable : 'true',
-  //     type : 'video',
-  //     part: 'snippet',
-  //     q : query,
-  //     maxResults : 5
-  //   })
-  //
-  //   .then(function successCallback(response)){
-  //       this.videos = response.items;
-  //     }, function errorCallback(response){
-  //       console.log('error: ', response);
-  //     });
-  // }
-  //
+// return {
+//   getData: function(){
+//     $http({
+//
+//     })
+//   },
+//   data: function(){ return myData; }
+// }
+//
+//
+//
+// this.getVideos = function(query){
+//
+//
+//
+//   $http({
+//     methods: 'GET',
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     key : YOUTUBE_API_KEY,
+//     videoEmbeddable : 'true',
+//     type : 'video',
+//     part: 'snippet',
+//     q : query,
+//     maxResults : 5
+//   })
+//
+//   .then(function successCallback(response)){
+//       this.videos = response.items;
+//     }, function errorCallback(response){
+//       console.log('error: ', response);
+//     });
+// }
+//
